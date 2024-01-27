8a45400b6eb07dade093f8fa296dd27a4bcfaa62
DianeHorly <modjomdiane@yahoo.com>
2024-01-27 20:25:44 +0000

    page index.html

--- Web_serveur/app.py
+++ Web_serveur/app.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+# serveur/app.py
 
-app= Flask(__name__)
+from flask import Flask, render_template, request
 
+app = Flask(__name__)
+
+# Route pour la page d'accueil
 @app.route('/')
-def home():
+def index():   
     return render_template('index.html')
 
-if __name__== '__main__':
-    app.run(host='0.0.0.0',port=8000, debug=True)
+
+# Route pour la page avec formulaire
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8000)
